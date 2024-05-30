6dccf1228ac1f7359f9cc1e6d6dc9d7251c898e6
Greg Courville <greg.courville@czbiohub.org>
2024-05-30 09:40:27 -0700

    add pyserial dependency

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setuptools.setup(
      name="reglo_icc_pump",
      version="0.1.0",
      python_requires=">=3.6",
+     install_requires=['pyserial'],  # TODO figure out min version
      py_modules=["reglo_icc_pump"],
      )
