79e99225b856c7e8c8e4071de8188d9077596f14
Greg Courville <greg.courville@czbiohub.org>
2024-06-18 18:26:30 -0700

    update usage example

--- docs/usage_example.py
+++ docs/usage_example.py
@@ -1,26 +1,42 @@
+import time
+
 from reglo_icc_pump import RegloIccPump
 
 
-p = RegloIccPump.from_serial_portname(
-    "COM1",
+print("Pumps are connected on these ports:")
+for portname in RegloIccPump.list_connected_devices():
+    print(f"  {portname!r}")
+
+print("\nSerial number check:")
+try:
+    foo = RegloIccPump.from_serial_portname(
+        "/dev/ttyACM0",
+        serial_no="WRONG1234"
+        )
+    print("  Okay!")
+except RegloIccPump.SerialNoMismatch as e:
+    print(f"  {e!r}")
+    time.sleep(0.2)
+
+print()
+p = RegloIccPump.open_first_device(
     dispense_dirs={1: "ccw", 3: "ccw"},
     tubing_ids={n: 1.52 for n in [1, 2, 3, 4]},
     )
-print(repr(p.model_no))
-print(repr(p.serial_no))
-print(repr(p.sw_ver))
-print(repr(p.head_code))
-print(repr(p.channel_nos))
-print(p.tubing_ids)
+print(f"Port name:        {p.ser_port.port!r}")
+print(f"Serial number:    {p.serial_no!r}")
+print(f"Firmware version: {p.sw_ver!r}")
+print(f"Pump head:        {p.head_code!r}")
+print(f"Channel numbers:  {p.channel_nos!r}")
 
 p.show_msg("Concurrent")
-print("Concurrent pumping operations")
+print("\nConcurrent pumping operations...")
 for ch_no in p.channel_nos:
-    p.aspirate_vol(ch_no, 2.5e-3, 0.5, blocking=False)
+    p.aspirate_vol(ch_no, 5.0e-3, 0.5, blocking=False)
 p.wait_for_stop()
 
 p.show_msg("Sequential")
-print("Sequential pumping operations")
+print("\nSequential pumping operations...")
 for ch_no in p.channel_nos:
     p.dispense_vol(ch_no, 42e-3, 10.0)
 p.show_msg("Done")
